a91ebe80b09834bd8fac88171b517c33bf43b0ba
zeuslearning-Devdoot-Maji <devdoot.maji@zeuslearning.com>
2024-02-20 22:59:12 +0530

    Updated the route for fetching Jobs

--- Client/src/app/services/jobs-listing-service/jobs-listing.service.ts
+++ Client/src/app/services/jobs-listing-service/jobs-listing.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 export class JobsListingService {
-    private apiUrl = 'http://localhost:5193/api/Job';
+    private apiUrl = 'http://localhost:5193/api/Jobs';
 
     constructor(private http: HttpClient) { }
 
